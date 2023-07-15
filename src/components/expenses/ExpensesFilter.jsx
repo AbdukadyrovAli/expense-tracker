e6839a8bf6cb821be0f42b11462942d3aa6cc754
Ali Abdukadyrov <vip.astiali@gmail.com>
2023-07-15 22:51:46 +0600

    added sort

--- src/components/expenses/ExpensesFilter.jsx
+++ src/components/expenses/ExpensesFilter.jsx
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpensesFilter.css";
 import Button from "../UI/button/Button";
 
-const ExpensesFilter = ({ selectedYear, onSelectedYearChange }) => {
+const ExpensesFilter = ({
+  renderedExpenses,
+  expenses,
+  setExpenses,
+  selectedYear,
+  onSelectedYearChange,
+}) => {
+  const [sortedExpenses, setSortedExpenses] = useState("");
+
+  const onSelectedButtonChange = (e) => {
+    setSortedExpenses(e.target.value);
+
+    if (sortedExpenses === "vniz") {
+      setExpenses([
+        ...expenses.sort((a, b) => {
+          return a.amount - b.amount;
+        }),
+      ]);
+    } else {
+      setExpenses([
+        ...expenses.sort((a, b) => {
+          return b.amount - a.amount;
+        }),
+      ]);
+    }
+    console.log(renderedExpenses);
+  };
+
+  const alfva = () => {
+    setExpenses([
+      ...expenses.sort((a, b) => {
+        return a.title.localeCompare(b.title);
+      }),
+    ]);
+  };
+  const newww = () => {
+    setExpenses([
+      ...expenses.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+      }),
+    ]);
+  };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
-        <select style={{backgroundColor:"#510674", color:"white"}}>
-          <option>
-            <Button>По возрастанию</Button>
-          </option>
-          <option>
-            <Button>По убыванию</Button>
-          </option>
+        <select
+          style={{ backgroundColor: "#510674", color: "white" }}
+          onChange={onSelectedButtonChange}
+          value={sortedExpenses}
+          id="filter"
+        >
+          <option value="vniz">По возрастанию</option>
+          <option value="verx">По убыванию</option>
         </select>
-        <Button>По новизне</Button>
-        <Button>По названию</Button>
+        <Button onClick={newww}>По новизне</Button>
+        <Button onClick={alfva}>По названию</Button>
         <label htmlFor="filter">Filter by year:</label>
 
         <select
