8c3a470d83a9ba9513d2992cb4d0d45be051b03b
Ali Abdukadyrov <vip.astiali@gmail.com>
2023-07-17 10:20:26 +0600

    expense finished

--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -3,26 +3,25 @@ import ExpenseItem from './ExpenseItem'
 import './ExpenseList.css'
 
 const ExpenseList = ({ expenses = [], onDeleteExpense }) => {
-	// const content =
-	// 	expenses.length === 0 ? (
-	// 		<h1 className='expenses-list__fallback'>No Expenses</h1>
-	// 	) : (
-	// <ul className='expenses-list'>
-	// 	{expenses.map((expense) => {
-	// 		return (
-	// 			<ExpenseItem
-	// 				onDeleteExpense={onDeleteExpense}
-	// 				date={expense.date}
-	// 				title={expense.title}
-	// 				id={expense.id}
-	// 				amount={expense.amount}
-	// 			/>
-	// 		)
-	// 	})}
-	// </ul>
-	// 	)
-	// return content
-	asdfgit 
+	const content =
+		expenses.length === 0 ? (
+			<h1 className='expenses-list__fallback'>No Expenses</h1>
+		) : (
+	<ul className='expenses-list'>
+		{expenses.map((expense) => {
+			return (
+				<ExpenseItem
+					onDeleteExpense={onDeleteExpense}
+					date={expense.date}
+					title={expense.title}
+					id={expense.id}
+					amount={expense.amount}
+				/>
+			)
+		})}
+	</ul>
+		)
+	return content
 
 	return (
 		<div>
